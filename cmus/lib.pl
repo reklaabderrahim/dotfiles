a65dd5b9db6768386bc28e0d6e627767bc522d7a
rekla abderrahim <reklaabderrahim@hotmail.com>
2022-01-29 17:47:17 +0100

    remove unwanted files

--- cmus/lib.pl
+++
@@ -1,5 +0,0 @@
-/home/rekla/Musique/Khaled - Didi (Didi Funk club remix).mp3
-/home/rekla/Musique/Khaled - Haya Haya.mp3
-/home/rekla/Musique/Khaled - Chebba.mp3
-/home/rekla/Musique/Khaled - C'Est La Vie.mp3
-/home/rekla/Musique/Khaled - Aïcha.mp3
